3e43b32fe0b29f71c1a2831c14651ee4fa35c16c
Dmytro Orlov <orlov.dm@gmail.com>
2020-11-22 22:18:03 +0100

    route

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -1,13 +1,29 @@
 package com.example
 
+import buildinfo.BuildInfo.version
 import distage.{Tag, _}
+import org.http4s.server.Router
+import sttp.tapir.docs.openapi._
+import sttp.tapir.openapi.circe.yaml._
+import sttp.tapir.swagger.http4s.SwaggerHttp4s
 import zio._
 import zio.console._
+import zio.interop.catz._
 
 object Main extends App {
   def run(args: List[String]) = {
     def provideHas[R: HasConstructor, A: Tag](fn: R => A): ProviderMagnet[A] =
       HasConstructor[R].map(fn)
+
+    val route = for {
+      implicit0(rts: Runtime[Any]) <- ZIO.runtime[Any]
+      add <- Endpoints.>.add
+      check <- Endpoints.>.check
+      docs = Seq(add, check).toOpenAPI("demo", version)
+      route = Router[Task](
+        "/" -> new SwaggerHttp4s(docs.toYaml).routes
+      )
+    } yield route
 
     val program =
       for {
