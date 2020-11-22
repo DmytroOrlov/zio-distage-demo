73b0edcab029ba4ed20b0dc1e58075a82400d11f
Dmytro Orlov <orlov.dm@gmail.com>
2020-11-22 22:01:32 +0100

    distage

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -1,5 +1,27 @@
 package com.example
 
+import distage.{Tag, _}
+import zio._
+import zio.console._
+
 object Main extends App {
-  println("123")
+  def run(args: List[String]) = {
+    def provideHas[R: HasConstructor, A: Tag](fn: R => A): ProviderMagnet[A] =
+      HasConstructor[R].map(fn)
+
+    val program =
+      for {
+        _ <- putStrLn("distage")
+      } yield ExitCode.success
+
+    val definition = new ModuleDef {
+      make[Console.Service].fromHas(Console.live)
+      make[UIO[ExitCode]].from(provideHas(program.provide))
+    }
+
+    Injector()
+      .produceGetF[Task, UIO[ExitCode]](definition)
+      .useEffect
+      .catchAll(e => UIO(println(s"failed to start $e")).as(ExitCode.failure))
+  }
 }
